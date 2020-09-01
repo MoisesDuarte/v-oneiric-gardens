72a77a66eec8e9b6b937394d962c560ea30e2f30
MoisesDuarte <moisesduartejunior@gmail.com>
2020-09-01 14:00:10 -0300

    Testing BootstrapVue component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="light" variant="light">
+    <b-navbar-brand href="#">
+        <img src="../assets/logo.svg" width="32px" class="align-top">
+        OneiricGardens
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Stories</b-nav-item>
+          <b-nav-item href="#">Categories</b-nav-item>
+          <b-nav-item href="#">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
