5d31f5fd966302328ccc2d1c669fea3a0470f782
MoisesDuarte <moisesduartejunior@gmail.com>
2020-09-01 14:06:27 -0300

    Testing router links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+<div id="navbar">
   <b-navbar toggleable="lg" type="light" variant="light">
-    <b-navbar-brand href="#">
+    <b-navbar-brand to="/">
         <img src="../assets/logo.svg" width="32px" class="align-top">
         OneiricGardens
     </b-navbar-brand>
@@ -10,9 +10,9 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#">Stories</b-nav-item>
-          <b-nav-item href="#">Categories</b-nav-item>
-          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item to="/stories">Stories</b-nav-item>
+          <b-nav-item to="/categories">Categories</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
