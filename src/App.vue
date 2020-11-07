275a61d8adcf02e60ffb8e3247cbc014ce56f189
MoisesDuarte <moisesduartejunior@gmail.com>
2020-11-06 22:12:37 -0300

    Adding transition to router change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view />
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>      
     <Footer />
   </div>
 </template>
@@ -23,5 +25,21 @@ export default {
 body {
   margin: 0;
   padding: 0;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-leave {
+  opacity: 0;
+}
+
+.fade-leave-active {
+  transition: opacity 1s;
 }
 </style>
